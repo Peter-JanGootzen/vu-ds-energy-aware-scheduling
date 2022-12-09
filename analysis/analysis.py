ba2d6f3c2f8077656e1352d5aa0e4fa4d7c29fda
Herman Kelder <hgkelder@upcmail.nl>
2022-12-09 14:35:24 +0100

    Restructured files into folder and added violin plots to the analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import seaborn as sns
 
 results_order = {
     "energy" : 0,
@@ -14,8 +15,18 @@ results_order = {
 }
 
 metric_label = {
-    "energy" : "Energy usage (kWh)"
+    "energy" : "Energy usage (kWh)",
+    "util" : "Utilisation"
 }
+
+# Inspired by https://matplotlib.org/stable/gallery/statistics/customized_violin.html
+def adjacent_values(vals, q1, q3):
+    upper_adjacent_value = q3 + (q3 - q1) * 1.5
+    upper_adjacent_value = np.clip(upper_adjacent_value, q3, vals[-1])
+
+    lower_adjacent_value = q1 - (q3 - q1) * 1.5
+    lower_adjacent_value = np.clip(lower_adjacent_value, vals[0], q1)
+    return lower_adjacent_value, upper_adjacent_value
 
 # Inspired by https://stackoverflow.com/a/60270421
 def bar_plot(ax, data, policies_names, workload_names, ylabel, colors=None, total_width=0.8, single_width=1, legend=True):
@@ -68,14 +79,14 @@ def read_results(results_path):
     return results
 
 
-def get_column_by_metric(results, metric):
+def get_metric(results, metric):
     return results[:, results_order[metric]]
 
 def get_avg_metric(results, metric):
-    return np.mean(get_column_by_metric(results, metric))
+    return np.mean(get_metric(results, metric))
 
 def get_std_metric(results, metric):
-    return np.std(get_column_by_metric(results, metric))
+    return np.std(get_metric(results, metric))
 
 
 def policies_metric(policies_result_paths, policies_names, metric):
@@ -123,54 +134,58 @@ def policies_workloads_barchart(policies_workloads_result_paths, policies_names,
         policies_results += [policy_results]
 
     bar_plot(ax, policies_results, policies_names, workload_names, metric_label[metric])
-    plt.savefig("result.pdf")
+    plt.savefig("results/result_{}.png".format(metric))
     plt.show()
 
-# Print single metric (avg + std) for multiple policies
+def policies_workloads_violin(policies_workloads_result_paths, policies_names, workload_names, metric, metric_range):
+    policies_results = []
+
+
+    for i, (policy_workloads_result_paths, policy_name) in enumerate(zip(policies_workloads_result_paths, policies_names)): # loop over policies
+        policy_results = []
+        for policy_workload_path in policy_workloads_result_paths:
+            results = read_results(policy_workload_path)
+            metric_results = get_metric(results, metric)
+            policy_results += [metric_results]
+
+        policies_results += [policy_results]
+
+
+    for i, workload in enumerate(workload_names):
+
+        fig, ax = plt.subplots(figsize=(10, 5))
+        workload_data = np.array(policies_results)[:, i]
+        sns.violinplot(ax=ax, data=list(workload_data), bw=0.15, orient="h")
+        # ax.violinplot(list(workload_data), [i for i in range(len(workload_data))], vert=False, showmedians=False, showextrema=False, showmeans=False)
+        # ax.boxplot(list(workload_data), [i for i in range(len(workload_data))], '')
+
+        ax.set_yticklabels(policies_names)
+        ax.set_xlabel(metric_label[metric])
+        ax.set_ylabel("Policy")
+        ax.set(xlim=metric_range)
+        ax.set_axisbelow(True)
+        ax.grid(axis='x')
+
+
+        plt.savefig("results/result_violin_{}.png".format(workload))
+        plt.show()
+
 policy_paths = ["experiment1/results1.csv", "experiment1/results2.csv"]
-policy_names = ["FCFS", "Random"]
-policies_metric(policy_paths, policy_names, "energy")
-
-
-# Print single metric (avg only) for multiple policies and multiple workloads
 policies_workloads_result_paths = [["experiment1/results1.csv", "experiment2/results1.csv", "experiment3/results1.csv"], ["experiment1/results2.csv", "experiment2/results2.csv", "experiment3/results2.csv"]]
 policy_names = ["FCFS", "Random"]
 workload_names = ["Alibaba", "bitbrains", "Google"]
+
+# Print single metric (avg + std) for multiple policies
+policies_metric(policy_paths, policy_names, "energy")
+
+# Print single metric (avg only) for multiple policies and multiple workloads
 policies_workload_metric(policies_workloads_result_paths, policy_names, workload_names, "energy")
+
+# Bar chart single metric (avg only) for multiple policies grouped on workloads
 policies_workloads_barchart(policies_workloads_result_paths, policy_names, workload_names, "energy")
 
-
-
-# def make_line_plots(generation_fitness_max, generation_fitness_mean, enemy, algo_num, folder_name='figures'):
-#     """
-#         Make line plots of max and mean fitness.
-#     """
-#     mean_means = [np.mean(gen) for gen in generation_fitness_mean]
-#     mean_stds  = [np.std(gen)  for gen in generation_fitness_mean]
-#     max_means  = [np.mean(gen) for gen in generation_fitness_max]
-#     max_stds   = [np.std(gen)  for gen in generation_fitness_max]
-
-#     mean_lows  = [mean_means[i] - mean_stds[i] for i in range(len(mean_means))]
-#     mean_highs = [mean_means[i] + mean_stds[i] for i in range(len(mean_means))]
-#     max_lows  = [max_means[i] - max_stds[i] for i in range(len(max_means))]
-#     max_highs = [max_means[i] + max_stds[i] for i in range(len(max_means))]
-
-#     best_fitness_generations, best_fitness_max = get_ascending_sublist(max_means)
-
-#     generations = np.arange(len(generation_fitness_mean))
-#     plt.errorbar(generations, mean_means, yerr=mean_stds, fmt='-o', label='Mean fitness', capsize=5, color='blue')
-#     plt.fill_between(generations, mean_lows, mean_highs, color='blue', alpha=0.5)
-#     plt.errorbar(generations, max_means,  yerr=max_stds,  fmt='-o', label='Maximum fitness', capsize=5, color='red')
-#     plt.fill_between(generations, max_lows, max_highs, color='red', alpha=0.5)
-#     plt.plot(best_fitness_generations, best_fitness_max, label='Mean best individual', color='black')
-#     plt.xlabel('Generation', fontsize=16)
-#     plt.ylabel('Fitness', fontsize=16)
-#     plt.title(f'Mean and maximum fitness plotted\nagainst generation for EA {algo_num} and enemy {enemy}',
-#               fontsize=22)
-#     plt.legend(prop={'size':14})
-#     plt.tight_layout()
-#     plt.savefig(f'{folder_name}/lineplot_enemy_{enemy}_EA_{algo_num}.pdf', format='pdf', bbox_inches='tight')
-#     plt.clf()
+# Violin plot single metric (avg only) for multiple policies on workload
+policies_workloads_violin(policies_workloads_result_paths, policy_names, workload_names, "util", (0, 1))
 
 
 
